1ab7f438b646217a739cfae2104c95014fb95a52
Joseph Hughes <joeydarlo1000@gmail.com>
2025-06-28 16:47:09 +0000

    create node function created

--- linkedList.c
+++ linkedList.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {}
+struct Node {
+  int data;
+  struct Node *next;
+};
+
+struct Node* createNode(int data) {
+  struct Node *newNode = malloc(sizeof(struct Node));
+  
+  newNode->data = data;
+  newNode->next = NULL;f
+  return newNode;
+}
+
+void insert(int data, struct Node** header){
+
+}
+
+void delete() {}
+
+int main() {
+    struct Node* header = createNode(0);
+}
