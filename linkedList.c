22895819a1773ee9c8a87eb7fda14595ea28fbe8
Joseph Hughes <joeydarlo1000@gmail.com>
2025-06-28 17:35:50 +0000

    Implemented print list func

--- linkedList.c
+++ linkedList.c
@@ -10,16 +10,35 @@ struct Node* createNode(int data) {
   struct Node *newNode = malloc(sizeof(struct Node));
   
   newNode->data = data;
-  newNode->next = NULL;f
+  newNode->next = NULL;
   return newNode;
 }
 
 void insert(int data, struct Node** header){
+    struct Node *newNode = malloc(sizeof(struct Node));
+    newNode->data = data;
 
+    newNode->next = *header; //this makes newNode point to the last thing header was pointing to
+    *header = newNode; //updates the header to include the new node 
+}
+
+void printList(struct Node* header) {
+    struct Node *current = header;
+
+    while (current != NULL) {
+        printf("%i", current->data);
+        current = current->next;
+    }
+    return;
 }
 
 void delete() {}
 
 int main() {
     struct Node* header = createNode(0);
+
+    insert(1, &header);
+
+    printList(header);
+    printf("Done");
 }
