2b256ab896122cd38619be9be02ced757c2d8411
Joseph Hughes <joeydarlo1000@gmail.com>
2025-07-05 21:59:50 +0000

    optimisations made

--- Vector/vector.c
+++ Vector/vector.c
@@ -1,40 +1,37 @@
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 struct dynamicArray {
-  int vector[0];
   int size;
   int capacity;
+  int vector[];
 };
 
 bool isEmpty(struct dynamicArray *vector) {
-  if (vector->capacity == 0) {
+  if (vector->size == 0) {
     return true;
   } else {
     return false;
   }
 }
 
-int size(struct dynamicArray *vector) { return vector->capacity; }
+int size(struct dynamicArray *vector) { return vector->size; }
 
 void clear(struct dynamicArray *vector) { free(vector); }
 
-void push_back(int value) {}
-
-struct dynamicArray front(struct dynamicArray *vector) {
-  if (!isEmpty((vector))) {
-    return vector[0];
-  }
+int front(struct dynamicArray *vector) {
+  return isEmpty(vector) ? -1 : vector->vector[0];
 }
 
-struct dynamicArray back(struct dynamicArray *vector) {
-  if (!isEmpty((vector))) {
-    return vector[vector->capacity];
-  }
+int back(struct dynamicArray *vector) {
+  return isEmpty(vector) ? -1 : vector->vector[vector->size - 1];
 }
 
 void resize(int newSize, struct dynamicArray *vector) {
+  int vectorSize = vector->size - 1;
+
   int *temp = malloc((newSize) * sizeof(int));
 
   for (int i = 0; i < newSize; i++) {
@@ -43,14 +40,12 @@ void resize(int newSize, struct dynamicArray *vector) {
 
   vector->capacity = newSize;
 
-  if (vector->size > newSize) {
-    vector->size = newSize;
+  if (vectorSize > newSize) {
+    vectorSize = newSize;
   }
 
   free(vector);
 }
-#include <stdio.h>
-#include <stdlib.h>
 
 int main() {
   // Allocate memory for your dynamicArray struct + 5 ints for vector
@@ -76,12 +71,12 @@ int main() {
   printf("size: %d\n", size(vec));
 
   // Test front()
-  struct dynamicArray front_val = front(vec);
-  printf("front: %d\n", front_val.vector[0]);
+  int front_val = front(vec);
+  printf("front: %d\n", front_val);
 
   // Test back()
-  struct dynamicArray back_val = back(vec);
-  printf("back: %d\n", back_val.vector[0]);
+  int back_val = back(vec);
+  printf("back: %d\n", back_val);
 
   // Test resize() - resize down to 3
   resize(3, vec);
