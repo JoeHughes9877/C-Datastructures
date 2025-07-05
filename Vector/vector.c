244b2000ea449655ad5f4dd8d1794129c57c9d2d
Joseph Hughes <joeydarlo1000@gmail.com>
2025-07-05 17:29:25 +0000

    implemented empty and size function

--- Vector/vector.c
+++ Vector/vector.c
@@ -1,3 +1,20 @@
+#include <stdbool.h>
 #include <stdio.h>
+
+struct Node {
+  int vector[0];
+  int size;
+  int capacity;
+};
+
+bool isEmpty(struct Node *vector) {
+  if (vector->capacity == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+int size(struct Node *vector) { return vector->capacity; }
 
 int main() {}
