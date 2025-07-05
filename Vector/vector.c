519a93a286d9ab3a964bfbb19e63ec4d28e9a296
Joseph Hughes <joeydarlo1000@gmail.com>
2025-07-05 19:26:15 +0000

    added resize func

--- Vector/vector.c
+++ Vector/vector.c
@@ -1,14 +1,15 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct Node {
+struct dynamicArray {
   int vector[0];
   int size;
   int capacity;
 };
 
-bool isEmpty(struct Node *vector) {
+bool isEmpty(struct dynamicArray *vector) {
   if (vector->capacity == 0) {
     return true;
   } else {
@@ -16,16 +17,36 @@ bool isEmpty(struct Node *vector) {
   }
 }
 
-int size(struct Node *vector) { return vector->capacity; }
+int size(struct dynamicArray *vector) { return vector->capacity; }
 
-void clear(struct Node *vector) { free(vector); }
+void clear(struct dynamicArray *vector) { free(vector); }
 
 void push_back(int value) {}
 
-struct Node front(struct Node *vector) {
+struct dynamicArray front(struct dynamicArray *vector) {
   if (!isEmpty((vector))) {
     return vector[0];
   }
 }
 
+struct dynamicArray back(struct dynamicArray *vector) {
+  if (!isEmpty((vector))) {
+    return vector[vector->capacity];
+  }
+}
+
+void resize(int newSize, struct dynamicArray *vector) {
+  int *temp = malloc((newSize) * sizeof(int));
+
+  for (int i = 0; i < newSize; i++) {
+    memcpy(&vector[i], &temp[i], sizeof(int));
+  }
+  free(vector);
+
+  vector->capacity = newSize;
+
+  if (vector->size > newSize) {
+    vector->size = newSize;
+  }
+}
 int main() {}
