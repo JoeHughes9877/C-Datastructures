c6d7ad5cc679c348f5b1b07d05f07d6dc2430fe2
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-07-23 22:23:11 +0100

    implemented push

--- stacks/stack.c
+++ stacks/stack.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 
-void push(int arr[]);
-int pop(int arr[]);
+struct Stack {
+  int arr[10];
+  int array_size;
+};
 
-void push(int arr[]) {}
+const int MAX_SIZE = 10;
 
-int pop(int arr[]) {}
+void push(struct Stack *s, int value);
+int pop(struct Stack *s);
+void print_stack();
 
-int main() { printf("silly"); }
+void push(struct Stack *s, int value) {
+  if (s->array_size + 1 == 11) {
+    printf("Stack full.");
+    return;
+  }
+
+  s->array_size++;
+  s->arr[s->array_size] = value;
+  printf("%i added to stack", value);
+  return;
+}
+
+void print_stack() {}
+
+int pop(struct Stack *s) {
+  int last_diget;
+
+  return last_diget;
+}
+
+int main() {
+  struct Stack s;
+  s.array_size = 0;
+
+  push(&s, 1);
+
+  return 0;
+}
